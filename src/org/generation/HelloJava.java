64d11fe3f313b8968ccb382afe4764d00eaaf679
luigilalo <luigilaloe1@gmail.com>
2023-11-28 13:04:16 +0100

    Added "HELLO JAITA99"

---
+++ src/org/generation/HelloJava.java
@@ -0,0 +1,10 @@
+package org.generation;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        // qui il codice eseguibile
+        System.out.println("Hello JAITA99");
+    }
+
+    }
